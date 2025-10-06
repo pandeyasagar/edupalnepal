da02a8c37eb5aa675ba039c7526c34c90a89feec
Sagar <sagarpandeya4@gmail.com>
2025-10-06 22:39:18 +0545

    Fix GH Pages base path and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
 import Quiz from "./Pages/Quiz";
 import PageNotFound from "./Pages/PageNotFound";
@@ -7,13 +7,13 @@ import "./Components/Homepage/Queries.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="quiz" element={<Quiz />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
